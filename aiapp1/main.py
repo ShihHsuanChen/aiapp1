6579b2066e8969117cc105e5acb3a6ff3ff2b4ca
ShihHsuanChen <jack072315@gmail.com>
2024-08-08 12:46:05 +0800

    CHANGE: read model name from configs

--- aiapp1/main.py
+++ aiapp1/main.py
@@ -1,12 +1,13 @@
 # pip install torch==2.2.1 torchvision==0.17.1 --index-url https://download.pytorch.org/whl/cu121
 from .model import load_model
 from .data import read_image
+from .configs import cfg
 
 # cli argument: myaiapp ./beignets-task-guide.png
 img = read_image('https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png')
 
 # configs: .env .ini .cfg <-- pydantic
-model = load_model('mobilenetv4_conv_small.e2400_r224_in1k')
+model = load_model(cfg.infer_model_name)
 
 # runtime interface: inputs -> (inference) -> output
 result = model.inference(img, topk=5)
